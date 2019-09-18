67150f0a0bafc3e5155778ed068d18c46ecc0cbc
Jordan Luong <jordanluong@live.com>
2019-09-19 00:43:21 +0900

    updated tweets field to an array

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,5 +3,11 @@ module Types
     field :id, ID, null: false
     field :username, String, null: false
     field :email, String, null: false
+    field :bio, String, null: true
+    field :tweets, [Types::TweetType], null: true
+
+    def tweets
+      Tweet.all
+    end
   end
 end
