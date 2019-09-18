710716a1e2fb0f5d01505e39d58028d44517e052
Jordan Luong <jordanluong@live.com>
2019-09-18 18:14:13 +0900

    added createdAt field to tweet type

--- app/graphql/types/tweet_type.rb
+++ app/graphql/types/tweet_type.rb
@@ -3,5 +3,6 @@ module Types
     field :id, ID, null: false
     field :content, String, null: false
     field :userId, ID, null: false
+    field :createdAt, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
